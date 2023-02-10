ad5ae1145093fcae302810322cb6c5868f1455f0
Danny Nagel <stanshady@gmail.com>
2023-02-10 13:40:12 -0500

    Replaced index and main with the no modules version

--- main.js
+++ main.js
@@ -1,37 +1,118 @@
-import{recamanInts, nextRecaman, generateRecaman} from "./recamanInts.js"
-import{makePath} from "./semiCircles.js"
+//Recaman Int Code
 
+let recamanInts = [];
 let displayInts = document.getElementById("displayInts");
-let nextBtn = document.getElementById("nextBtn")
-nextBtn.addEventListener('click', nextRecamanAndPrint)
-let generateBtn = document.getElementById("generateBtn")
-generateBtn.addEventListener('click', generateRecamanAndPrint)
-let animateBtn = document.getElementById("animateBtn")
-animateBtn.addEventListener('click', drawPath)
+let prevAdded;
+let switchDir;
+
+//returns true if added, false if subtracted
+function nextRecaman() {
+  let size = recamanInts.length;
+  if(size == 0){
+    recamanInts.push(0);
+    prevAdded = true;
+    switchDir = false;
+    return true;
+  }
+
+  let lastRecaman = recamanInts[size - 1];
+  let temp = lastRecaman - size;
+  
+  //add
+  if (temp < 0 || recamanInts.includes(temp)) {
+      recamanInts.push(lastRecaman + size);
+      switchDir = prevAdded ? true : false;
+      prevAdded = true;
+
+      return true;
+
+  //subtract    
+  } else {
+      recamanInts.push(temp);
+      switchDir = prevAdded ? false : true;
+      prevAdded = false;
+      
+      return false;
+  }
+}
 
 function nextRecamanAndPrint(){
   nextRecaman();
+  changeDisplayInts();
+}
+
+function generateRecaman() {
+  recamanInts = [];
+  let n = document.getElementById("nth").value;
+  
+  if(n <= 0){
+    return;
+  }
+
+  for (let i = 1; i <= n; i++) {
+      nextRecaman();
+  }
+  changeDisplayInts();
+}
+
+function changeDisplayInts(){
   displayInts.textContent = recamanInts;
 }
 
-function generateRecamanAndPrint(){
-  generateRecaman();
-  displayInts.textContent = recamanInts;
+
+//Semicircle code:
+
+let elem = document.getElementById("semicircles");
+
+function makePath(){
+  let path = "M 10 350";
+
+  let clockwise = 1;
+  let rScale = 2;
+  let radius;
+  let dx;
+
+  let n = recamanInts.length;
+  recamanInts = [];
+  
+  for(let i = 0; i < n; i++){
+    let added = nextRecaman();
+    clockwise = switchDir ? clockwise = (clockwise + 1) % 2 : clockwise;
+    radius = rScale * (recamanInts.length-1);
+  
+    if(added){
+      dx = radius*2;  
+    }
+    else{
+      
+      dx = radius*-2;
+    }
+  
+    path += makePathStr(radius, clockwise, dx);
+  }
+
+  elem.setAttribute("d", path);
+  elem.style.strokeDashoffset = elem.getTotalLength();
+  elem.style.strokeDasharray = elem.getTotalLength(); 
+}
+
+function makePathStr(radius, clockwise, dx){
+   return "a" + radius + " " + radius + " " + 180 + " 1 " + clockwise + " " + dx + " 0";
 }
 
 function drawPath(){
   makePath();
-  let finalPath = document.getElementById("semicircles");
-  let len = finalPath.getTotalLength();
+
+  let len = elem.getTotalLength();
   let id = setInterval(frame, 10);
   function frame(){
-    if (len < 0){
+    if (len <= 0){
       clearInterval(id);
-      finalPath.style.strokeDashoffset = 0;
     }
     else {
       len -= 20;
-      finalPath.style.strokeDashoffset = len;
+      elem.style.strokeDashoffset = len;
     }  
   }
 }
+
